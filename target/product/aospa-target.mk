85edd7da91575532162b8abbf642333b8a81260c
Dylan AKPINAR <github@dylanakp.dev>
2024-03-13 17:19:05 +0100

    build: SomethingOS name as output

--- target/product/aospa-target.mk
+++ target/product/aospa-target.mk
@@ -126,10 +126,7 @@ SYSTEM_OPTIMIZE_JAVA := true
 SYSTEMUI_OPTIMIZE_JAVA := true
 
 #Lawnchair
-ifneq ($(TARGET_USES_LAWNCHAIR), true)
-PRODUCT_COPY_FILES += \
-   $(call inherit-product-if-exists, vendor/lawnchair/lawnchair.mk)
-endif
+$(call inherit-product-if-exists, vendor/lawnchair/lawnchair.mk)
 
 # Microsoft
 $(call inherit-product, vendor/aospa/prebuilt/microsoft/packages.mk)
