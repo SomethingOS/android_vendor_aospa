40d9c2bd9027ac0d47a2b6d47630bc501b02dde8
DylanAkp <github@dylanakp.dev>
2024-04-19 21:03:01 +0200

    bootanimation: Add Bootanimation themes [2/3]

--- bootanimation/bootanimation.mk
+++ bootanimation/bootanimation.mk
@@ -5,4 +5,8 @@
 #
 
 # Bootanimation
-PRODUCT_COPY_FILES += vendor/aospa/bootanimation/bootanimation.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+PRODUCT_COPY_FILES += \
+	vendor/aospa/bootanimation/SomethingOS.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip \
+	vendor/aospa/bootanimation/Dots.zip:$(TARGET_COPY_OUT_PRODUCT)/media/dots.zip \
+	vendor/aospa/bootanimation/Googlish.zip:$(TARGET_COPY_OUT_PRODUCT)/media/googlish.zip \
+	vendor/aospa/bootanimation/Pixel.zip:$(TARGET_COPY_OUT_PRODUCT)/media/pixel.zip
