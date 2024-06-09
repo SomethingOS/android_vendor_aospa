18db3dc5f82bb748bd597368bd7a2d637d91d640
DylanAkp <github@dylanakp.dev>
2024-06-09 13:52:29 +0200

    Paranoid: drop ParanoidPapers

--- target/product/aospa-target.mk
+++ target/product/aospa-target.mk
@@ -139,7 +139,6 @@ $(call inherit-product-if-exists, vendor/aospa/translations/translations.mk)
 
 # Paranoid Packages
 PRODUCT_PACKAGES += \
-    ParanoidPapers \
     ParanoidSystemUI \
     ParanoidThemePicker
 
