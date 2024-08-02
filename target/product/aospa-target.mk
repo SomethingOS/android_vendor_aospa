065be733614290e4726768c7a675cfd6336164f5
DylanAkp <github@dylanakp.dev>
2024-08-02 17:31:02 +0200

    Misc: Enable freeform window

--- target/product/aospa-target.mk
+++ target/product/aospa-target.mk
@@ -84,6 +84,10 @@ PRODUCT_COPY_FILES += \
 
 $(call inherit-product, external/google-fonts/lato/fonts.mk)
 
+# Freeform Window
+PRODUCT_COPY_FILES += \
+frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.freeform_window_management.xml
+
 # Gestures
 PRODUCT_PACKAGES += \
     vendor.aospa.power-service
