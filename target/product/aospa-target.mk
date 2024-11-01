c88be764f393a254b70be8dee7c575ae7295e242
Dylan AKPINAR <github@dylanakp.dev>
2024-11-01 16:28:21 +0100

    Prebuilts: Remove Abstruct
    Change-Id: I21a6fa7c3544090aaa42d7813cf9a9795746ea68

--- target/product/aospa-target.mk
+++ target/product/aospa-target.mk
@@ -19,10 +19,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Some CTS test case failed after enabling feature config_stopSystemPackagesByDefault
 PRODUCT_PACKAGES += initial-package-stopped-states-aosp.xml
 
-# Abstruct
-PRODUCT_PACKAGES += \
-    Abstruct
-
 # AOSPA Version.
 $(call inherit-product, vendor/aospa/target/product/version.mk)
 
