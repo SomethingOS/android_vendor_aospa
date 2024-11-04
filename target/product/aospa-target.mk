b39f208b3e6141d7cd8f71f0efbc670ae1806db5
Dylan AKPINAR <github@dylanakp.dev>
2024-11-04 09:34:25 +0100

    Target: Don't build ParanoidPapers
    Change-Id: I950b6b145b426b6ff3a32635c9e045923bdee640

--- target/product/aospa-target.mk
+++ target/product/aospa-target.mk
@@ -132,7 +132,7 @@ $(call inherit-product-if-exists, vendor/aospa/translations/translations.mk)
 
 # Paranoid Packages
 PRODUCT_PACKAGES += \
-    ParanoidPapers \
+    ParanoidSettings \
     ParanoidSystemUI \
     ParanoidThemePicker
 
